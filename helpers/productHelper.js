eaff863646cf7fe6cff15c546a46caaa5235f3e1
mdsahal <mdsahalpp@gmail.com>
2024-09-19 15:20:26 +0530

    admin side addProduct and view product are completed

--- helpers/productHelper.js
+++ helpers/productHelper.js
@@ -1,17 +1,21 @@
-const { resolve, reject } = require('promise');
+
 var db = require('../config/connection');
 var collection = require('../config/collection')
 module.exports ={
     addProduct:(product,callback)=>{
         db.get().collection('product').insertOne(product).then((data)=>{
-            callback(data.ops[0]._id)
+            console.log("Inserted id:" + data.insertedId)
+            callback(data.insertedId)
         }).catch((err) => {
-            callback(false)
+            console.log("error during insertion : "+err)
         })
-    },getAllProducts: async ()=>{
+    },
+    getAllProducts: async ()=>{
         return new Promise((resolve,reject)=>{
             let products=db.get().collection(collection.PRODUCT_COLLECTION).find().toArray()
+            console.log(products)
             resolve(products)
         })
     }
 };
+
--- routes/admin.js
+++ routes/admin.js
@@ -4,27 +4,30 @@ var productHelper = require('../helpers/productHelper');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  productHelper.getAllProducts().then((products)=>{
-    console.log("Saadhanam kayyilund")
-    res.render('admin/viewProducts',{admin:true, products})
+  productHelper.getAllProducts().then((products) => {
+    console.log(products)
+    res.render('admin/viewProducts', { admin: true, products })
   })
 });
 
-router.get('/addProduct',function(req,res){
-  console.log("get working")
+router.get('/addProduct', function(req, res) {
   res.render('admin/addProduct')
-  
+
 });
 
-router.post('/addProduct',(req,res)=>{
+router.post('/addProduct', (req, res) => {
+  console.log("req:" + req.body + "\n");
 
-  productHelper.addProduct(req.body,(id)=>{
+  productHelper.addProduct(req.body, (id) => {
+    console.log("id from admin: " + id)
     let image = req.files.image;
-    id = Date.now() + '.jpg';
-    image.mv('./public/product-image/'+id+'.jpg',(err,done)=>{
-      if(!err){
+    console.log(image)
+    let imageId = id.toString();
+    console.log("iamge id: " + imageId);
+    image.mv('./public/product-image/' + imageId + '.jpg', (err, done) => {
+      if (!err) {
         res.render('admin/addProduct');
-      }else{
+      } else {
         console.log(err)
       }
     })
